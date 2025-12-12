a3b369f4002d1b1323debd0c289b9ac1c5689327
Kevin <kobbayi@gmail.com>
2025-12-11 16:04:22 -0800

    Initial Commit

---
+++ FoodPin/View/RestaurantDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  RestaurantDetailView.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 30/10/2025.
+//
+
+import SwiftUI
+
+struct RestaurantDetailView: View {
+    
+    var restaurant: Restaurant
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            Image(restaurant.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .ignoresSafeArea()
+            
+            Color.black
+                .frame(height: 100)
+                .opacity(0.8)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+                .overlay {
+                    VStack(spacing: 5) {
+                        Text(restaurant.name)
+                        Text(restaurant.type)
+                        Text(restaurant.location)
+                    }
+                    .font(.system(.headline, design: .rounded))
+                    .foregroundStyle(.white)
+                }
+        }
+    }
+}
+
+#Preview {
+    RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
+}
