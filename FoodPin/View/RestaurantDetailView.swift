53438600e5a811a54aa6016ff67c9ef5052dde35
Kevin <kobbayi@gmail.com>
2025-12-12 18:51:17 -0800

    Completed chp 12 sans the bug fix

--- FoodPin/View/RestaurantDetailView.swift
+++ FoodPin/View/RestaurantDetailView.swift
@@ -12,31 +12,68 @@ struct RestaurantDetailView: View {
     var restaurant: Restaurant
     
     var body: some View {
-        ZStack(alignment: .top) {
-            Image(restaurant.image)
-                .resizable()
-                .scaledToFill()
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .ignoresSafeArea()
-            
-            Color.black
-                .frame(height: 100)
-                .opacity(0.8)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .padding()
-                .overlay {
-                    VStack(spacing: 5) {
+        ScrollView {
+            VStack (alignment: .leading) {
+                Image(restaurant.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                //  .ignoresSafeArea()
+                    .frame(height: 445)
+            }
+            .overlay {
+                VStack {
+                    Image(systemName: "heart")
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+                        .padding()
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                        .padding(.top, 40)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
                         Text(restaurant.name)
+                            .font(.custom("Nunito-Regular", size: 35, relativeTo: .largeTitle))
+                            .bold()
                         Text(restaurant.type)
-                        Text(restaurant.location)
+                            .font(.system(.headline, design: .rounded))
+                            .padding(.all, 5)
+                            .background(Color.black)
                     }
-                    .font(.system(.headline, design: .rounded))
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
                     .foregroundStyle(.white)
+                    .padding()
                 }
+            }
+            
+            Text(restaurant.description)
+                .padding()
+            //
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("ADDRESS")
+                        .font(.system(.headline, design: .rounded))
+
+                    Text(restaurant.location)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+
+                VStack(alignment: .leading) {
+                    Text("PHONE")
+                        .font(.system(.headline, design: .rounded))
+
+                    Text(restaurant.phone)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal)
+            
+            
         }
+        .ignoresSafeArea()
     }
 }
 
+
 #Preview {
-    RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
+    RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. \n\nWe open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: true))
 }
