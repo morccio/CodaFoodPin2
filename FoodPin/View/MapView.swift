28b811f77b4da09e2bbdc3a49b7e17850fb0a262
Kevin <kobbayi@gmail.com>
2025-12-19 00:44:28 -0800

    updated file

--- FoodPin/View/MapView.swift
+++ FoodPin/View/MapView.swift
@@ -11,13 +11,13 @@ import MapKit
 struct MapView: View {
     
     var location: String = ""
-
-    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+    //let geoCoder = CLGeocoder()
+    
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:  40.75773, longitude: -73.985708), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
     
     var body: some View {
-        //
+  
         Map(initialPosition: .region(region))
-        
     }
 }
 
