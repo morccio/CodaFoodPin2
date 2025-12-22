5ccc078e911ee7b17adfb8925e907dee0acff51b
Kevin <kobbayi@gmail.com>
2025-12-21 16:28:11 -0800

    Syncing

--- FoodPin/View/MapView.swift
+++ FoodPin/View/MapView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 struct MapView: View {
     
@@ -14,9 +15,17 @@ struct MapView: View {
 
     @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
     
+    let geocoder = CLGeocoder()
+    
+    
     var body: some View {
         //
-        Map(initialPosition: .region(region))
+        //Map(initialPosition: .region(region))
+        Map(initialPosition:
+              .region(MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: 40.75773, longitude: -73.985708),
+                        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+              )))
         
     }
 }
