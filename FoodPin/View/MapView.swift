a1b8d4b3620ac05fab2583c283572c7e09d4521f
Kevin <kobbayi@gmail.com>
2025-12-22 22:14:45 -0800

    Completed Map Vew chapter

--- FoodPin/View/MapView.swift
+++ FoodPin/View/MapView.swift
@@ -9,27 +9,50 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    
     var location: String = ""
-    //let geoCoder = CLGeocoder()
     
-    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude:  40.75773, longitude: -73.985708), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+  //  @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.75773, longitude: -73.985708), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
-    let geocoder = CLGeocoder()
-    
+    @State private var position: MapCameraPosition = .automatic
+    @State private var markerLocation = CLLocation()
     
     var body: some View {
-        //
-        //Map(initialPosition: .region(region))
-        Map(initialPosition:
-              .region(MKCoordinateRegion(
-                        center: CLLocationCoordinate2D(latitude: 40.75773, longitude: -73.985708),
-                        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-              )))
         
+        Map(position: $position) {
+            Marker("Here", coordinate: markerLocation.coordinate)
+                .tint(.red)
+        }
+        .task {
+            convertAddress(location: location)
+        }
+         
+        //Map(initialPosition: .region(region))
+    }
+    
+    
+    private func convertAddress(location: String) {
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let placemarks = placemarks,
+               let location = placemarks[0].location else {
+                return
+            }
+            
+            let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.0015, longitudeDelta: 0.0015))
+            
+            self.position = .region(region)
+            self.markerLocation = location
+        })
     }
 }
 
 #Preview {
-    MapView()
+   
+    MapView(location: "1308 5th Avenue, Seattle, WA 98101, USA")
 }
