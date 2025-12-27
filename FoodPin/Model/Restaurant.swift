442819660d1fcd98b117fd0dee4f8b30c0dd0509
Kevin <kobbayi@gmail.com>
2025-12-26 16:27:39 -0800

    changed resturant struct to class

--- FoodPin/Model/Restaurant.swift
+++ FoodPin/Model/Restaurant.swift
@@ -6,25 +6,19 @@
 //
 
 import Foundation
+import Combine
 
-struct Restaurant {
-    var name: String
-    var type: String
-    var location: String
-    var phone: String
-    var description: String
-    var image: String
-    var isFavorite: Bool
-    
-    init(name: String, type: String, location: String, phone: String, description: String, image: String, isFavorite: Bool = false) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.phone = phone
-        self.description = description
-        self.image = image
-        self.isFavorite = isFavorite
-    }
+class Restaurant: ObservableObject {
+
+    @Published var name: String
+    @Published var type: String
+    @Published var location: String
+    @Published var phone: String
+    @Published var description: String
+    @Published var image: String
+    @Published var isFavorite: Bool = false
+    @Published var rating: Rating?
+
     
     enum Rating: String, CaseIterable {
         case awesome
@@ -42,9 +36,18 @@ struct Restaurant {
             case .terrible: return "angry"
             }
         }
+
     }
+
     
-    init() {
-        self.init(name: "", type: "", location: "", phone: "", description: "", image: "", isFavorite: false)
+    init(name: String, type: String, location: String, phone: String, description: String, image: String, isFavorite: Bool = false, rating: Rating? = nil) {
+            self.name = name
+            self.type = type
+            self.location = location
+            self.phone = phone
+            self.description = description
+            self.image = image
+            self.isFavorite = isFavorite
+            self.rating = rating
+        }
     }
-}
