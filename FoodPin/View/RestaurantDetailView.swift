b33d5098705e7544098651562c1cc026c9163a5b
Kevin <kobbayi@gmail.com>
2026-01-05 21:39:02 -0800

    completed ratings feature

--- FoodPin/View/RestaurantDetailView.swift
+++ FoodPin/View/RestaurantDetailView.swift
@@ -42,23 +42,31 @@ struct RestaurantDetailView: View {
                         .foregroundColor(.white)
                         .padding(.top, 40)
                     }
-                   
-                   // Map()
                     
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(restaurant.name)
-                            .font(.custom("Nunito-Regular", size: 35, relativeTo: .largeTitle))
-                            .bold()
-                        Text(restaurant.type)
-                            .font(.system(.headline, design: .rounded))
-                            .padding(.all, 5)
-                            .background(Color.black)
+                    HStack(alignment: .bottom) {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(restaurant.name)
+                                .font(.custom("Nunito-Regular", size: 35, relativeTo: .largeTitle))
+                                .bold()
+                            Text(restaurant.type)
+                                .font(.system(.headline, design: .rounded))
+                                .padding(.all, 5)
+                                .background(Color.black)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+                        .foregroundStyle(.white)
+                        .padding()
+                        
+                        
+                        if let rating = restaurant.rating, !showReview {
+                            Image(rating.image)
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                                .padding([.bottom, .trailing])
+                                .transition(.scale)
+                        }
                     }
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
-                    .foregroundStyle(.white)
-                    .padding()
-                    
-                   
+                    .animation(.spring(response: 0.2, dampingFraction: 0.3, blendDuration: 0.3), value: restaurant.rating)
                     
                     
                 }
