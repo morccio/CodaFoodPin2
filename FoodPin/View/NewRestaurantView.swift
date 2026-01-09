2df36f8c037782458e8e15780256c726bca27d7d
Kevin <kobbayi@gmail.com>
2026-01-09 11:10:39 -0800

    Updated

--- FoodPin/View/NewRestaurantView.swift
+++ FoodPin/View/NewRestaurantView.swift
@@ -32,6 +32,43 @@ struct NewRestaurantView: View {
     }
 }
 
+
+
+struct FormTextField: View {
+    
+    let label: String
+    var placeholder: String = ""
+
+    @Binding var value: String
+
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text(label.uppercased())
+                .font(.system(.headline, design: .rounded))
+                .foregroundStyle(Color(.darkGray))
+
+            TextField(placeholder, text: $value)
+                .font(.system(.body, design: .rounded))
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color(.systemGray5), lineWidth: 1)
+                )
+                .padding(.vertical, 10)
+
+        }
+    }
+}
+
+
+
 #Preview {
     NewRestaurantView()
+}
+
+#Preview("FormTextField", traits: .fixedLayout(width: 300, height: 200)) {
+    FormTextField(label: "NAME 2", placeholder: "Fill in the restaurant name", value: .constant(""))
 }
