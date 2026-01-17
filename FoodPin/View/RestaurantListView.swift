940ac0a60ef310ab12bc865b6eea82f5816f4f68
Kevin <kobbayi@gmail.com>
2026-01-17 00:31:34 -0800

    Added the launch new restaurant form button

--- FoodPin/View/RestaurantListView.swift
+++ FoodPin/View/RestaurantListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct RestaurantListView: View {
     
+    @State private var showNewRestaurant = false
+    
     @State var restaurants = [
                                 Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: false),
                                Restaurant(name: "Homei", type: "Cafe", location: "Shop B, G/F, 22-24A Tai Ping San Street SOHO, Sheung Wan, Hong Kong", phone: "348-233423", description: "A little gem hidden at the corner of the street is nothing but fantastic! This place is warm and cozy. We open at 7 every morning except Sunday, and close at 9 PM. We offer a variety of coffee drinks and specialties including lattes, cappuccinos, teas, and more. We serve breakfast, lunch, and dinner in an airy open setting. Come over, have a coffee and enjoy a chit-chat with our baristas.", image: "homei", isFavorite: false),
@@ -72,7 +74,19 @@ struct RestaurantListView: View {
             
             .navigationTitle("FoodPin")
             .navigationBarTitleDisplayMode(.automatic)
-        }
+            
+            .toolbar {
+                Button(action: {
+                    self.showNewRestaurant = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            }
+            
+            }
+            .sheet(isPresented: $showNewRestaurant) {
+                NewRestaurantView()
+            }
         
     }
 }
